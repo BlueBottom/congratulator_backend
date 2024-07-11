3695c3f42530497573cb208d672a022ca9488943
Tatyana Lutsishin <xxdelfinchikxx@gmail.com>
2024-07-11 09:51:07 +0300

    Add abstractions

---
+++ Congratulator.Core/Abstractions/IBirthdayRepository.cs
@@ -0,0 +1,12 @@
+﻿using Congratulator.Domain.Birthday;
+
+namespace Congratulator.Core.Abstractions;
+
+public interface IBirthdayRepository
+{
+    Task<Guid> Create(Birthday birthday);
+    Task<Guid> Delete(Guid id);
+    Task<List<Birthday>> Get(string intervalTime, string searchString);
+    Task<Guid> Update(Guid id, string name, string description, DateTime date, byte[] image);
+    
+}
