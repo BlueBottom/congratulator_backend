985b4397e774f9fa939ed0d71fd5deb2964eebf3
Tatyana Lutsishin <xxdelfinchikxx@gmail.com>
2024-07-11 18:17:11 +0300

    Add IBirthdayService implementation

---
+++ Congratulator.Application/Services/BirthdayService.cs
@@ -0,0 +1,35 @@
+﻿using Congratulator.Contracts.Contracts;
+using Congratulator.Core.Abstractions;
+using Congratulator.Domain.Birthday;
+
+namespace Congratulator.Application.Services
+{
+    public class BirthdayService : IBirthdayService
+    {
+        private readonly IBirthdayRepository _birthdayRepository;
+        public BirthdayService(IBirthdayRepository birthdayRepository)
+        {
+            _birthdayRepository = birthdayRepository;
+        }
+
+        public async Task<List<Birthday>> GetAllBirthdays(string intervalTime = "", string searchString = "")
+        {
+            return await _birthdayRepository.Get(intervalTime, searchString);
+        }
+
+        public async Task<Guid> CreateBirthday(Birthday birthday)
+        {
+            return await _birthdayRepository.Create(birthday);
+        }
+
+        public async Task<Guid> UpdateBirthday(Guid id, string name, string description, DateTime date, byte[] image)
+        {
+            return await _birthdayRepository.Update(id, name, description, date, image);
+        }
+
+        public async Task<Guid> DeleteBirthday(Guid id)
+        {
+            return await _birthdayRepository.Delete(id);
+        }
+    }
+}
