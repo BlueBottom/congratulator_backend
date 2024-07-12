18fabbfd287d32fa191ff33ae9f85f941ee6d573
Tatyana Lutsishin <xxdelfinchikxx@gmail.com>
2024-07-12 08:32:42 +0300

    Add contracts

---
+++ Congratulator.Contracts/Contracts/BirthdayResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Congratulator.API.Contracts
+{
+    public record BirthdayResponse(
+        Guid Id, 
+        string Name,
+        string Description,
+        DateTime Date,
+        byte[] Image
+         );
+}
